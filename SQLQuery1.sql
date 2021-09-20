f1a50cf7c6f4f70e830746781bbddfa824223821
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-20 11:27:28 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+Create Database Restaurant;
+use Restaurant 
+Create Table resto(
+rset_id int not null Primary key,
+name varchar (50),
+Open_time int not null,
+close_time int not null,
+Phoneno Bigint not null,
+Address varchar(200),
+Cuisine varchar(50),
+Status varchar(20)
+)
+select *from resto;
+drop table resto;
